fdc79045d2b37324ea5444b98abdab0b7c22fe14
Clara Oliveira <claraoliveira.8351@gmail.com>
2025-12-08 16:54:56 -0300

    Create FreteServiceTest.java
    Adicionados o teste das classe FreteService

---
+++ src/test/java/com/projeto/FreteServiceTest.java
@@ -0,0 +1,34 @@
+package com.projeto;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import com.projeto.frete.FreteService;
+
+public class FreteServiceTest {
+
+    FreteService service = new FreteService();
+
+    @Test
+    void deveCalcularFreteCorretamente() {
+        double resultado = service.calcularFrete(10);
+        assertEquals(25.0, resultado);
+    }
+
+    @Test
+    void deveLancarErroParaDistanciaNegativa() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.calcularFrete(-5);
+        });
+    }
+
+    @Test
+    void deveRetornarFreteGratisQuandoCompraMaiorOuIgual200() {
+        assertTrue(service.freteGratis(200));
+        assertTrue(service.freteGratis(250));
+    }
+
+    @Test
+    void deveRetornarFreteNaoGratisQuandoCompraMenorQue200() {
+        assertFalse(service.freteGratis(199.90));
+    }
+}
